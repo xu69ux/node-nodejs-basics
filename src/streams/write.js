77f533b3e56cfcaabd13dd5bc9f5d53a29a2752a
xu69ux <spiridonova.kseniya@gmail.com>
2024-01-22 15:53:28 +0600

    feat: streams (src/streams) done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream('src/streams/files/fileToWrite.txt');
+    process.stdin.on('data', (chunk) => {
+        writeStream.write(chunk);
+    });
 };
 
 await write();

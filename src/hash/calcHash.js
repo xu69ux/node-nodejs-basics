a4c8ebb270130adf62c8f88216df9b8e76111321
xu69ux <spiridonova.kseniya@gmail.com>
2024-01-22 15:40:01 +0600

    feat: hash (src/hash) done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from 'crypto';
+import fs from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream('src/hash/files/fileToCalculateHashFor.txt');
+    
+    stream.on('data', (data) => {
+        hash.update(data);
+    });
+
+    stream.on('end', () => {
+        const result = hash.digest('hex');
+        console.log(result);
+    })
 };
 
 await calculateHash();
